f9caaca732aa9531b37aae575a7ed736a7af677a
Carrington <Manyuchi>
2023-08-16 14:03:53 +0200

    adding ui

--- ScrollView+Login/LoginVC.swift
+++ ScrollView+Login/LoginVC.swift
@@ -27,11 +27,20 @@ class LoginVC: UIViewController {
     private let headingLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Login"
+        label.text = "Welcome"
         label.textColor = UIColor.label
         label.font = UIFont(name: "avenir", size: 25)
         label.textAlignment = .center
-        
+        return label
+    }()
+    
+    private let headingTwoLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Sign in and have fun"
+        label.textColor = UIColor.label
+        label.font = UIFont(name: "avenir", size: 25)
+        label.textAlignment = .center
         return label
     }()
     
@@ -43,6 +52,42 @@ class LoginVC: UIViewController {
         return imageView
     }()
     
+    private let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Enter username"
+        textField.layer.cornerRadius = 20
+        textField.layer.shadowOffset = CGSize(width: 2, height: 1)
+        textField.layer.shadowOpacity = 0.5
+        textField.layer.shadowColor = UIColor.black.cgColor
+        textField.backgroundColor = .systemGray5
+        
+        let leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 25.0, height: 0.0))
+        textField.leftView = leftView
+        textField.rightView = leftView
+        textField.leftViewMode = .always
+        textField.rightViewMode = .always
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "Enter password"
+        textField.layer.cornerRadius = 20
+        textField.layer.shadowOffset = CGSize(width: 2, height: 1)
+        textField.layer.shadowOpacity = 0.5
+        textField.layer.shadowColor = UIColor.black.cgColor
+        textField.backgroundColor = .systemGray5
+        
+        let leftView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 25.0, height: 0.0))
+        textField.leftView = leftView
+        textField.rightView = leftView
+        textField.leftViewMode = .always
+        textField.rightViewMode = .always
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -50,6 +95,8 @@ class LoginVC: UIViewController {
         loginScrollView.addSubview(contentView)
         contentView.addSubview(headingLabel)
         contentView.addSubview(headingImage)
+        contentView.addSubview(usernameTextField)
+        contentView.addSubview(passwordTextField)
         configureConstraints()
     }
     
@@ -84,16 +131,30 @@ class LoginVC: UIViewController {
         let  composeHeadingImage = [
             headingImage.topAnchor.constraint(equalTo: headingLabel.bottomAnchor, constant: 50),
             headingImage.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            
-            
             headingImage.heightAnchor.constraint(equalToConstant: 350),
             headingImage.widthAnchor.constraint(equalToConstant: 200)
+        ]
+        
+        let composeUsernameTextField = [
+            usernameTextField.topAnchor.constraint(equalTo: headingImage.bottomAnchor, constant: 35),
+            usernameTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 25),
+            usernameTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -25),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 40)
+        ]
+        
+        let composePasswordTextField = [
+            passwordTextField.topAnchor.constraint(equalTo: usernameTextField.bottomAnchor, constant: 25),
+            passwordTextField.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 25),
+            passwordTextField.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -25),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 40)
         ]
         
         NSLayoutConstraint.activate(composeloginScrollView)
         NSLayoutConstraint.activate(composeContentView)
         NSLayoutConstraint.activate(composeHeadingLabel)
         NSLayoutConstraint.activate(composeHeadingImage)
+        NSLayoutConstraint.activate(composeUsernameTextField)
+        NSLayoutConstraint.activate(composePasswordTextField)
     }
 
 }
